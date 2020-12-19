217967553b345939325c65532b6b676fe6adac9b
Restyled.io <commits@restyled.io>
2020-12-19 09:26:03 +0000

    Restyled by astyle

--- src/m1sdr.c
+++ src/m1sdr.c
@@ -12,11 +12,13 @@ m1sdr_callback_t *m1sdr_Callback;
 ao_bool hw_present;
 
 void m1sdr_SetCallback(m1sdr_callback_t *function) {
-  if (function == NULL) {
-    printf("ERROR: NULL CALLBACK!\n");
-  }
+    if (function == NULL) {
+        printf("ERROR: NULL CALLBACK!\n");
+    }
 
-  m1sdr_Callback = function;
+    m1sdr_Callback = function;
 }
 
-ao_bool m1sdr_HwPresent(void) { return hw_present; }
+ao_bool m1sdr_HwPresent(void) {
+    return hw_present;
+}
