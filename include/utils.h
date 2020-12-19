b16bc1618bf2278e55eaaed5ff1dc8fb5890021c
CartoonFan <jeremiahwilson777@gmail.com>
2020-12-19 01:26:32 -0800

    chore: Make some changes to prep for meson (#2)
    * chore: update submodules
    
    * chore: Make some changes to prep for meson
    
    * Restyle chore: Make some changes to prep for meson (#3)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle chore: Make some changes to prep for meson (#4)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle chore: Make some changes to prep for meson (#5)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle chore: Make some changes to prep for meson (#6)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle chore: Make some changes to prep for meson (#7)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle chore: Make some changes to prep for meson (#8)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle chore: Make some changes to prep for meson (#9)
    
    * Restyled by astyle
    
    * Restyled by clang-format
    
    * Update cpuintrf.h
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: CartoonFan <jeremiahwilson777@gmail.com>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- include/utils.h
+++ include/utils.h
@@ -16,30 +16,30 @@
 // desired data size by calling hashtable_init().
 
 typedef struct {
-	void *buf;
-	size_t len;
+  void *buf;
+  size_t len;
 } blob_t;
 
 typedef struct hashtable_bucket {
-	struct hashtable_bucket *next;
-	blob_t key;
-	uint8 data[0];
+  struct hashtable_bucket *next;
+  blob_t key;
+  uint8 data[0];
 } hashtable_bucket_t;
 
 typedef struct {
-	unsigned int i;
-	hashtable_bucket_t *bucket;
+  unsigned int i;
+  hashtable_bucket_t *bucket;
 } hashtable_iterator_t;
 
 typedef struct {
-	size_t data_size;
-	hashtable_bucket_t *buckets;
+  size_t data_size;
+  hashtable_bucket_t *buckets;
 } hashtable_t;
 
 // Flags used for hash table lookup
 typedef enum {
-	HT_CREATE = 0x1, // create nonexisting entries
-	HT_CASE_INSENSITIVE = 0x2, // case-insensitive key lookup
+  HT_CREATE = 0x1,           // create nonexisting entries
+  HT_CASE_INSENSITIVE = 0x2, // case-insensitive key lookup
 } hashtable_flags_t;
 
 // Initializes the hash table with the given data size.
@@ -48,13 +48,15 @@ ao_bool hashtable_init(hashtable_t *table, size_t data_size);
 // Looks up the entry in [table] with the given [key] and the given [flags].
 // Returns a writable pointer to the entry data, or NULL if the entry doesn't
 // exist and the HT_CREATE flag was not given.
-void* hashtable_get(hashtable_t *table, const blob_t *key, hashtable_flags_t flags);
+void *hashtable_get(hashtable_t *table, const blob_t *key,
+                    hashtable_flags_t flags);
 
 // Iterates over all entries in [table], using [iter] to store the iteration
 // state.  [iter] should be cleared to 0 before the first call. Returns the
 // current data entry as well as the [key] if that pointer is not NULL, or
 // NULL if the end of [table] has been reached.
-void* hashtable_iterate(blob_t **key, hashtable_t *table, hashtable_iterator_t *iter);
+void *hashtable_iterate(blob_t **key, hashtable_t *table,
+                        hashtable_iterator_t *iter);
 
 // Returns the number of entries in [table].
 unsigned int hashtable_length(hashtable_t *table);
@@ -65,6 +67,6 @@ void hashtable_free(hashtable_t *table);
 /// ----------
 
 // Opens "[fn].[suffix]" for writing.
-FILE* fopen_derivative(const char *fn, const char *suffix);
+FILE *fopen_derivative(const char *fn, const char *suffix);
 
 #endif /* UTILS_H */
