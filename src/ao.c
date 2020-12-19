774b83fb896f22902146dcdb55f9c4369133cb8b
Restyled.io <commits@restyled.io>
2020-12-19 09:26:04 +0000

    Restyled by clang-format

--- src/ao.c
+++ src/ao.c
@@ -12,24 +12,22 @@
 #include <unistd.h>
 #endif
 
-FILE *ao_fopen(const char *fn, const char *mode) {
-    return fopen(fn, mode);
-}
+FILE *ao_fopen(const char *fn, const char *mode) { return fopen(fn, mode); }
 
 int ao_mkdir(const char *dirname) {
 #ifdef WIN32
-    // Let's avoid the entire problem of certain runtimes omitting the
-    // mode parameter altogether by just using the kernel function.
-    return CreateDirectoryA(dirname, NULL);
+  // Let's avoid the entire problem of certain runtimes omitting the
+  // mode parameter altogether by just using the kernel function.
+  return CreateDirectoryA(dirname, NULL);
 #else
-    return mkdir(dirname, 0777);
+  return mkdir(dirname, 0777);
 #endif
 }
 
 void ao_sleep(unsigned int msecs) {
 #ifdef WIN32
-    Sleep(msecs);
+  Sleep(msecs);
 #else
-    usleep((useconds_t)(msecs)*1000);
+  usleep((useconds_t)(msecs)*1000);
 #endif
 }
